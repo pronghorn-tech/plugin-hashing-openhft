30e5cca2f4ba1edb4649daf3d345ae2802e5e5e7
Chris Turner <chris@pronghorn.tech>
2018-03-11 13:41:05 -0600

    added visibility modifier

--- src/main/kotlin/tech/pronghorn/plugins/OpenHFTArrayHasherPlugin.kt
+++ src/main/kotlin/tech/pronghorn/plugins/OpenHFTArrayHasherPlugin.kt
@@ -19,7 +19,7 @@ package tech.pronghorn.plugins
 import net.openhft.hashing.LongHashFunction
 import tech.pronghorn.plugins.arrayHash.ArrayHasherPlugin
 
-object OpenHFTArrayHasherPlugin : ArrayHasherPlugin {
+public object OpenHFTArrayHasherPlugin : ArrayHasherPlugin {
     private val hasher = LongHashFunction.farmNa()
     override fun get(): (ByteArray) -> Long = hasher::hashBytes
 }
